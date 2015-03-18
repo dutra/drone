05a0c863923add9ffad6a08e42626862795bdebb
Isaque Dutra <dutra@mit.edu>
2015-03-18 11:58:29 -0400

    Added spi_read_bytes to spi.c

--- src/imu.c
+++ src/imu.c
@@ -17,20 +17,15 @@ int8_t imu_init() {
     spi_init(SPI_MODE3);
 
     // read WHO_AM_I_G register
-    PORTA &= ~(1<<CS_G_BIT);
-    spi_transmit(0x80 | (0x3F & WHO_AM_I_G));
-    whoami = spi_transmit(0x00);
-    PORTA |= (1<<CS_G_BIT);
+    whoami = spi_read_byte(CS_G_BIT, WHO_AM_I_G);
     if(whoami != 0xD4)
         return 1;
 
     // read WHO_AM_I_XM
-    PORTA &= ~(1<<CS_XM_BIT);
-    spi_transmit(0x80 | (0x3F & WHO_AM_I_XM));
-    whoami = spi_transmit(0x00);
-    PORTA |= (1<<CS_XM_BIT);
+    whoami = spi_read_byte(CS_XM_BIT, WHO_AM_I_XM);
     if(whoami != 0x49)
-         return 2;
+        return 2;
 
+//    read_byte(whoami);
     return 0;
 }
--- src/spi.c
+++ src/spi.c
@@ -6,6 +6,18 @@
 void spi_init(SpiMode spi_mode) {
     DDRB |= (1<<MOSI_BIT) | (1<<SCK_BIT) | (1<<SS_BIT);
     DDRB &= ~(1<<MISO_BIT);
+
+    /* SPCR (SPI Control Register)
+    SPCR[7:0]: SPIE SPE DORD MSTR CPOL CPHA SPR1 SPR0
+            SPIE: SPI Interrupt Enable
+            SPE:  SPI Enable
+            DORD: Data Order. MSB first when 1.
+            MSTR: Master/Slave Select. Master when 1.
+            CPOL: Clock Polarity. Clock is high when idle if CPOL 1.
+            CPHA: Clock Phase. CPHA 0 means sample on leading clock edge,
+                        while CPHA 1 means sample on trailing clock edge.
+            SPR[1:0]: SPI Clock Rate Select.
+    */
     SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR0);
 
     switch(spi_mode) {
@@ -28,7 +40,35 @@ void spi_init(SpiMode spi_mode) {
 
 }
 uint8_t spi_transmit(uint8_t byte) {
+
+    // For writing, set MSB high
+    // For reading, set MSB low
+    // For a multiple byte transmission, set bit 6 high
     SPDR = byte;
     while (!(SPSR & (1<<SPIF))) ; // wait
     return SPDR;
+}
+
+uint8_t spi_read_byte(uint8_t cs_pin, uint8_t sub_address) {
+    uint8_t tmp;
+
+    spi_read_bytes(cs_pin, sub_address, &tmp, 1);
+
+    return tmp;
+}
+
+void spi_read_bytes(uint8_t cs_pin, uint8_t sub_address, uint8_t *dest, uint8_t count) {
+
+    PORTA &= ~(1<<cs_pin);
+
+    if (count > 1)
+        spi_transmit(0xC0 | (sub_address & 0x3F));
+    else
+        spi_transmit(0x80 | (sub_address & 0x3F));
+
+    for (int i=0; i<count; i++)
+        dest[i] = spi_transmit(0x00);
+
+    PORTA |= (1<<cs_pin);
+
 }
